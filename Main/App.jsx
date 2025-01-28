cd3e1f2d914e56ab46eff35ef9a0deaf3d5afe83
devaarasi-yokesh <160761753+devaarasi-yokesh@users.noreply.github.com>
2025-01-28 19:48:57 +0530

    Create App.jsx

---
+++ Main/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Home from './Home'
+import Projects from './Projects'
+import Skills from './Skills'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <Home/>
+      <Projects/>
+      <Skills />
+    </>
+  )
+}
+
+export default App
